8cb3c83a43d3f3547605b5232c739eafc1aef2ff
Kendall Bingham <binghamkl@umkc.edu>
2022-11-23 17:26:34 -0600

    Initial creation and beginning

---
+++ GDriveApp/GDriveCopy.py
@@ -0,0 +1,27 @@
+import os
+import time
+from datetime import datetime
+
+# Module to copy files from a source directory to a Google drive folder if they've been updated.
+
+
+def SyncAndCopyFiles(Source : str, Destination : str) : 
+    '''
+        Syncs all source files into Destination on google drive.
+    '''
+    print(Source, flush=True)
+    print(Destination, flush=True)
+
+
+    files = os.listdir(Source)
+
+    fh = open(os.path.join(Destination, "filelist.txt"), "w")
+    fh.write(Source + "\n")
+    fh.write(Destination + "\n")
+    fh.write ("\n".join(files))
+    fh.close
+
+source = os.getenv("Source", ".")
+destination = os.getenv("Destination", ".")
+
+SyncAndCopyFiles(source, destination)
